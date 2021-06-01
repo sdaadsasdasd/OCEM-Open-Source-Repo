094c78eefcae84a140c48098d19ef3324d668143
wabsol <blackmoon640@gmail.com>
2021-06-01 21:28:01 +0200

    moved to right branch

---
+++ Conncet-ed/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    float xInput;               // Player a/d movement
+    float zInput;               // Player w/s movement
+    public float speed;         // Speed multiplier to default values
+    public Rigidbody rb;
+
+    void Update()
+    {
+        /*// Delta time ensures that player doesn't move faster at higher framerates
+
+        // Move the player left-to-right
+        xInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * xInput * Time.deltaTime * speed);
+
+        // Move the player forward or backwards
+        zInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * zInput * Time.deltaTime * speed);*/
+
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(xInput*speed, 0, zInput*speed,ForceMode.Impulse);
+    }
+}
